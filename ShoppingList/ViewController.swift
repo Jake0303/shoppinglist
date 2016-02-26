f0924f716c4f9dc6d680bfd809d36fd969249020
student <student@students-mac.student.georgianc.on.ca>
2016-02-26 11:43:39 -0500

    First commit

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var shoppingListTextField: UITextField!
+    @IBOutlet weak var newListItem1TextField: UITextField!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var quantityLabel: UILabel!
+    var quantityNum = Int32()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func minusButtonClick(sender: AnyObject) {
+        quantityNum = Int32(quantityLabel.text!)!
+        if(quantityNum > 0)
+        {
+            quantityNum--
+            quantityLabel.text = String(quantityNum)
+        }
+    }
 
+    @IBAction func addButtonClick(sender: AnyObject) {
+        quantityNum = Int32(quantityLabel.text!)!
+        quantityNum++
+        quantityLabel.text = String(quantityNum)
+        
+
+    }
+    @IBAction func cancelButtonClick(sender: AnyObject) {
+        quantityNum = 0
+        quantityLabel.text = String(quantityNum)
+    }
 }
 
